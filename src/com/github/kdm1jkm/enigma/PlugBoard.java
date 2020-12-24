82722ff4324c1ce9343847df134000e240f496ca
kdm <kdm1jkmm@gmail.com>
2020-12-24 15:12:40 +0900

    Add plugboard

---
+++ src/com/github/kdm1jkm/enigma/PlugBoard.java
@@ -0,0 +1,48 @@
+package com.github.kdm1jkm.enigma;
+
+public class PlugBoard {
+    private final char[] plugs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
+    public String convert(String str) {
+        StringBuilder result = new StringBuilder();
+        for (char ch : str.toCharArray()) {
+            result.append(convert(ch));
+        }
+        return result.toString();
+    }
+
+    public char convert(char ch) {
+        if (!(charToInt(ch) < plugs.length)) {
+            throw new IllegalArgumentException();
+        }
+
+        return plugs[charToInt(ch)];
+    }
+
+    public void deletePlug(char ch) {
+        if (!isAllocated(ch)) {
+            return;
+        }
+
+        char otherChar = plugs[charToInt(ch)];
+        plugs[charToInt(otherChar)] = otherChar;
+        plugs[charToInt(ch)] = ch;
+    }
+
+    public void addPlug(char c1, char c2) {
+        if (isAllocated(c1) || isAllocated(c2)) {
+            throw new IllegalArgumentException();
+        }
+
+        plugs[charToInt(c1)] = c2;
+        plugs[charToInt(c2)] = c1;
+    }
+
+    private int charToInt(char ch) {
+        return ch - 'A';
+    }
+
+    private boolean isAllocated(char ch) {
+        return plugs[charToInt(ch)] != ch;
+    }
+}
---
+++ test/com/github/kdm1jkm/enigma/PlugBoardTest.java
@@ -0,0 +1,51 @@
+package com.github.kdm1jkm.enigma;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PlugBoardTest {
+    @Test
+    void convertString() {
+        PlugBoard board = new PlugBoard();
+        String str = "ABCDEFG";
+        board.addPlug('A', 'G');
+        board.addPlug('B', 'H');
+
+        assertEquals("GHCDEFA", board.convert(str));
+    }
+
+    @Test
+    void convertChar() {
+        PlugBoard board = new PlugBoard();
+        for (char ch : "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray()) {
+            assertEquals(ch, board.convert(ch));
+        }
+    }
+
+    @Test
+    void deletePlug() {
+        PlugBoard board = new PlugBoard();
+        board.addPlug('A', 'C');
+        board.addPlug('B', 'D');
+        board.addPlug('F', 'G');
+        board.deletePlug('B');
+        board.deletePlug('A');
+
+        assertEquals('A', board.convert('A'));
+        assertEquals('B', board.convert('B'));
+        assertEquals('C', board.convert('C'));
+        assertEquals('D', board.convert('D'));
+
+        assertEquals('G', board.convert('F'));
+    }
+
+    @Test
+    void addPlug() {
+        PlugBoard board = new PlugBoard();
+        board.addPlug('A', 'C');
+        assertEquals('C', board.convert('A'));
+        assertEquals('A', board.convert('C'));
+        assertEquals('B', board.convert('B'));
+    }
+}
